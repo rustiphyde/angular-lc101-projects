045db9156318a20ab3bafe8dc0398c7d5111e1a9
rustiphyde <rustiphyde@gmail.com>
2019-10-20 21:46:36 -0500

    Part 1 of Lesson 1 Execises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = '../../assets/sandwich-fire-logo.png';
+  image3 = '../../assets/travelers_lantern.png';
 
   constructor() { }
 
