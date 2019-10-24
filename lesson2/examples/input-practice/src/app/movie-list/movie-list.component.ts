72f54e7eee3674fdd0ecc35a3b0717afafddca87
rustiphyde <rustiphyde@gmail.com>
2019-10-23 22:17:49 -0500

    Lesson 2 Part 3 Input Practice

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -5,11 +5,25 @@ import { Component, OnInit } from '@angular/core';
    templateUrl: './movie-list.component.html',
    styleUrls: ['./movie-list.component.css']
 })
+
 export class MovieListComponent implements OnInit {
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
 
    constructor() { }
 
    ngOnInit() {
+   }
+
+   addMovie(newTitle: string) {
+      let message = '';
+      if (newTitle === '') {
+         message = 'Please enter a movie title.';
+      } if (this.movies.includes(newTitle)){
+         message = `${newTitle} is already in your movie watchlist.`;
+      }
+      else {
+         this.movies.push(newTitle);
+      }
+      return message;
    }
 }
